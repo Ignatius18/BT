892e362271e120d72e90c5e48fdf26db7aa27132
Ignatius18 <bebrochka60yearsold@gmail.com>
2026-04-12 21:06:47 +0300

    feat(config): add main application entry point

---
+++ src/main/java/com/mybyteclub/byteclub/ByteclubApplication.java
@@ -0,0 +1,13 @@
+package com.mybyteclub.byteclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteclubApplication.class, args);
+	}
+
+}
