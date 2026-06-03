57dd9cc788b48350d2aa52f8d6fd2e321efd3c1c
Ignatius18 <bebrochka60yearsold@gmail.com>
2026-06-03 10:32:25 +0300

    feat: add methods to find user by email and username or email

--- src/main/java/com/mybyteclub/byteclub/repository/UserRepository.java
+++ src/main/java/com/mybyteclub/byteclub/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.mybyteclub.byteclub.repository;
 
-import com.mybyteclub.byteclub.model.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import com.mybyteclub.byteclub.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
