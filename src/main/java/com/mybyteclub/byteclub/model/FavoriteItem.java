62c157b8448f604577e89747212dc999089ef411
Ignatius18 <bebrochka60yearsold@gmail.com>
2026-06-03 10:31:17 +0300

    feat: create FavoriteItem entity for favorite products

---
+++ src/main/java/com/mybyteclub/byteclub/model/FavoriteItem.java
@@ -0,0 +1,54 @@
+package com.mybyteclub.byteclub.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "favorite_items",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"user_id", "product_id"})
+        })
+public class FavoriteItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    public FavoriteItem() {}
+
+    public FavoriteItem(Long userId, Long productId) {
+        this.userId = userId;
+        this.productId = productId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+} 
